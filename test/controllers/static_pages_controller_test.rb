67220fbe0c1edc9141f7cdf473f6f2a72de80266
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-03-16 12:45:01 +0300

    Test root

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,8 +6,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
